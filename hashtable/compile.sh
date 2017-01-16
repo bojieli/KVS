2cd8d2e71ce36576188dd35c8fc54a342039d8d6
Zhenyuan Ruan <zainryan95@gmail.com>
2017-01-16 05:27:38 -0800

    modify compile.sh to create bin dir when it doesn't exist in unittest folder

--- hashtable/compile.sh
+++ hashtable/compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d "bin" ]; then
+    mkdir bin;
+fi
+
 cp src/kernels.cl src/kernels.cpp;
 g++ -g -lrt -lboost_system -lboost_thread -lpthread src/kernels.cpp -o bin/kernels.out;
 rm src/kernels.cpp;
